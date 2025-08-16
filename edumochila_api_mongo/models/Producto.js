777a316d35914ac6be6b3c6e938c5d415abbee9b
Victor <victorlopezcebreros117@gmail.com>
2025-08-16 13:41:01 -0600

    render comienzo

--- edumochila_api_mongo/models/Producto.js
+++ edumochila_api_mongo/models/Producto.js
@@ -1,13 +1,14 @@
 import mongoose from 'mongoose';
 
 const ProductoSchema = new mongoose.Schema({
-  producto_id:   { type: String, required: true, unique: true, index: true },
-  user_id:       { type: Number, required: true, index: true }, // id_us del JWT
-  fecha_registro:{ type: Date,   required: true, default: Date.now }
-}, { collection: 'productos', timestamps: false });
+  producto_id:    { type: String, required: true },   // <- sin index/unique aquí
+  user_id:        { type: Number, required: true },   // <- sin index aquí
+  fecha_registro: { type: Date,   default: Date.now }
+}, { collection: 'productos', timestamps: false, versionKey: false });
 
-// Índices útiles
+// índices (uno por línea, sin duplicar)
 ProductoSchema.index({ producto_id: 1 }, { unique: true });
-ProductoSchema.index({ user_id: 1 }); // si quieres 1 por usuario, hazlo unique:true
+// Si quieres permitir solo 1 producto por usuario, pon unique:true aquí
+ProductoSchema.index({ user_id: 1 /*, unique: true */ });
 
 export default mongoose.model('Producto', ProductoSchema);
