51c02623cafa77152e1634e5d594b26e40402d12
Victor <victorlopezcebreros117@gmail.com>
2025-08-20 05:49:09 -0600

    ya

--- edumochila_api_mysql/models/UsuarioProducto.js
+++ edumochila_api_mysql/models/UsuarioProducto.js
@@ -1,23 +1,26 @@
+// models/UsuarioProducto.js
 import { DataTypes } from 'sequelize';
 import { sequelize } from '../config/database.js';
 
 const UsuarioProducto = sequelize.define('usuario_productos', {
   id_us: {
     type: DataTypes.INTEGER,
-    allowNull: true,
+    allowNull: false,
+    primaryKey: true,          // 👈 evita que Sequelize agregue 'id'
   },
   producto_id: {
     type: DataTypes.STRING(10),
-    allowNull: true
+    allowNull: false,
+    primaryKey: true,          // 👈 PK compuesta (id_us + producto_id)
   },
   nom_alumno: {
     type: DataTypes.STRING(100),
-    allowNull: true
+    allowNull: true,
   }
 }, {
   tableName: 'usuario_productos',
   timestamps: false,
-  freezeTableName: true
+  freezeTableName: true,
 });
 
 export default UsuarioProducto;
