c70282ff9d71597f489ff14d94da84732771eaf5
Victor <victorlopezcebreros117@gmail.com>
2025-08-20 11:10:41 -0600

    web semifinal 3

--- edumochila_api_mysql/models/UsuarioProducto.js
+++ edumochila_api_mysql/models/UsuarioProducto.js
@@ -3,24 +3,28 @@ import { DataTypes } from 'sequelize';
 import { sequelize } from '../config/database.js';
 
 const UsuarioProducto = sequelize.define('usuario_productos', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false
+  },
   id_us: {
     type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,          // 👈 evita que Sequelize agregue 'id'
+    allowNull: false   // la tabla lo tiene NOT NULL
   },
   producto_id: {
     type: DataTypes.STRING(10),
-    allowNull: false,
-    primaryKey: true,          // 👈 PK compuesta (id_us + producto_id)
+    allowNull: false   // la tabla lo tiene NOT NULL
   },
   nom_alumno: {
     type: DataTypes.STRING(100),
-    allowNull: true,
+    allowNull: true
   }
 }, {
   tableName: 'usuario_productos',
   timestamps: false,
-  freezeTableName: true,
+  freezeTableName: true
 });
 
 export default UsuarioProducto;
