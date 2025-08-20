a12408cfed0aa2917b358299d043d3dbac2da9dc
Victor <victorlopezcebreros117@gmail.com>
2025-08-20 03:54:39 -0600

    alumnos web 6

--- edumochila_api_mysql/routes/alumnos.routes.js
+++ edumochila_api_mysql/routes/alumnos.routes.js
@@ -1,12 +1,11 @@
 // routes/alumnos.routes.js
 import { Router } from 'express';
-import { authGuard } from '../middlewares/authGuard.js';
 import { crearAlumno, listarAlumnos, eliminarAlumno } from '../controllers/alumnos.controller.js';
 
 const router = Router();
 
-router.get('/alumnos', authGuard, listarAlumnos);
-router.post('/alumnos', authGuard, crearAlumno);
-router.delete('/alumnos/:producto_id', authGuard, eliminarAlumno);
+router.get('/alumnos', listarAlumnos);
+router.post('/alumnos', crearAlumno);
+router.delete('/alumnos/:producto_id', eliminarAlumno);
 
 export default router;
